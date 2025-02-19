17b62e2914e0dfeee816b358ecc972f3f66e2420
Alessandro Scolavino <scolas@gmail.com>
2025-02-19 08:33:48 +0100

    Migrating to php 8.4

--- class.phpwsdlparam.php
+++ class.phpwsdlparam.php
@@ -2,18 +2,18 @@
 
 /*
 PhpWsdl - Generate WSDL from PHP
-Copyright (C) 2011  Andreas Müller-Saala, wan24.de 
+Copyright (C) 2011  Andreas Mï¿½ller-Saala, wan24.de
 
-This program is free software; you can redistribute it and/or modify it under 
-the terms of the GNU General Public License as published by the Free Software 
-Foundation; either version 3 of the License, or (at your option) any later 
-version. 
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later
+version.
 
-This program is distributed in the hope that it will be useful, but WITHOUT 
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
-FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. 
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with 
+You should have received a copy of the GNU General Public License along with
 this program; if not, see <http://www.gnu.org/licenses/>.
 */
 
@@ -25,46 +25,46 @@ PhpWsdl::RegisterHook('InterpretKeywordreturnHook','internal','PhpWsdlParam::Int
 
 /**
  * A parameter or return value definition for a method
- * 
- * @author Andreas Müller-Saala, wan24.de
+ *
+ * @author Andreas Mï¿½ller-Saala, wan24.de
  */
 class PhpWsdlParam extends PhpWsdlObject{
 	/**
 	 * The parameter type name
-	 * 
+	 *
 	 * @var string
 	 */
 	public $Type;
 	/**
 	 * Documentation
-	 * 
+	 *
 	 * @var string
 	 */
 	public $Docs=null;
 	/**
 	 * The default name for the return value object
 	 * Use %method% as placeholder for the method name
-	 * 
+	 *
 	 * @var string
 	 */
 	public static $DefaultReturnName='return';
-	
+
 	/**
 	 * Constructor
-	 * 
+	 *
 	 * @param string $name The name
 	 * @param string $type Optional the type name (default: string)
 	 * @param array $settings Optional the settings hash array (default: NULL)
 	 */
-	public function PhpWsdlParam($name,$type='string',$settings=null){
+	public function __construct($name,$type='string',$settings=null){
 		PhpWsdl::Debug('New parameter '.$name);
 		parent::PhpWsdlObject($name,$settings);
 		$this->Type=$type;
 	}
-	
+
 	/**
 	 * Create the part WSDL
-	 * 
+	 *
 	 * @param PhpWsdl $pw The PhpWsdl object
 	 * @return string The WSDL
 	 */
@@ -81,10 +81,10 @@ class PhpWsdlParam extends PhpWsdlObject{
 		}
 		return $res;
 	}
-	
+
 	/**
 	 * Create parameter HTML documentation
-	 * 
+	 *
 	 * @param array $data Some data
 	 */
 	public function CreateParamHtml($data){
@@ -104,10 +104,10 @@ class PhpWsdlParam extends PhpWsdlObject{
 			$data
 		);
 	}
-	
+
 	/**
 	 * Create return value HTML documentation
-	 * 
+	 *
 	 * @param array $data Some data
 	 */
 	public function CreateReturnHtml($data){
@@ -128,10 +128,10 @@ class PhpWsdlParam extends PhpWsdlObject{
 			$data
 		);
 	}
-	
+
 	/**
 	 * Interpret a parameter keyword
-	 * 
+	 *
 	 * @param array $data The parser data
 	 * @return boolean Response
 	 */
@@ -141,21 +141,21 @@ class PhpWsdlParam extends PhpWsdlObject{
 		$info=explode(' ',$data['keyword'][1],3);
 		if(sizeof($info)<2)
 			return true;
-		$name=(substr($info[1],0,1)=='$')?substr($info[1],1):$info[1];
-		if(substr($name,strlen($name)-1,1)==';')
+		$name=(str_starts_with($info[1], '$'))?substr($info[1],1):$info[1];
+		if(str_ends_with($name, ';'))
 			$name=substr($name,0,strlen($name)-1);
 		PhpWsdl::Debug('Interpret parameter '.$name);
-		if($data['server']->ParseDocs)
+        if(isset($data['server']) && $data['server']->ParseDocs)
 			if(sizeof($info)>2)
 				$data['settings']['docs']=trim($info[2]);
 		$data['param'][]=new PhpWsdlParam($name,$info[0],$data['settings']);
 		$data['settings']=Array();
 		return false;
 	}
-	
+
 	/**
 	 * Interpret a return value
-	 * 
+	 *
 	 * @param array $data The parser data
 	 * @return boolean Response
 	 */
@@ -166,7 +166,7 @@ class PhpWsdlParam extends PhpWsdlObject{
 		if(sizeof($info)<1)
 			return true;
 		PhpWsdl::Debug('Interpret return');
-		if($data['server']->ParseDocs)
+        if(isset($data['server']) && $data['server']->ParseDocs)
 			if(sizeof($info)>1)
 				$data['settings']['docs']=trim($info[1]);
 		$data['return']=new PhpWsdlParam(str_replace('%method%',$data['method'],self::$DefaultReturnName),$info[0],$data['settings']);
--- class.phpwsdlproxy.php
+++ class.phpwsdlproxy.php
@@ -2,27 +2,27 @@
 
 /*
 PhpWsdl - Generate WSDL from PHP
-Copyright (C) 2011  Andreas Müller-Saala, wan24.de 
+Copyright (C) 2011  Andreas MÃ¼ller-Saala, wan24.de
 
-This program is free software; you can redistribute it and/or modify it under 
-the terms of the GNU General Public License as published by the Free Software 
-Foundation; either version 3 of the License, or (at your option) any later 
-version. 
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later
+version.
 
-This program is distributed in the hope that it will be useful, but WITHOUT 
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
-FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. 
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with 
+You should have received a copy of the GNU General Public License along with
 this program; if not, see <http://www.gnu.org/licenses/>.
 */
 
 if(basename($_SERVER['SCRIPT_FILENAME'])==basename(__FILE__))
 	exit;
 
-// This proxy can handle missing parameters with NULL values. This will only 
-// work, if the PhpWsdl class doesn't promote the WSDL to the SoapServer. But 
-// then returning complex types won't be easy anymore: You have to encode the 
+// This proxy can handle missing parameters with NULL values. This will only
+// work, if the PhpWsdl class doesn't promote the WSDL to the SoapServer. But
+// then returning complex types won't be easy anymore: You have to encode the
 // return value with PHPs SoapVar object by yourself.
 
 class PhpWsdlProxy{
